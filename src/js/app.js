291e97430979ca33634fc4f9fef0eb379e078fc7
Nikson01 <honcharenkomykyta@gmail.com>
2020-05-07 18:36:37 +0300

    rebuilded

--- src/js/app.js
+++ src/js/app.js
@@ -24,49 +24,39 @@ $(document).ready(function () {
   });
 
 
-
   // Styles for slick sliders
-
   $('.single-item').slick({
 		infinite: true,
     arrows: true,
-    nextArrow: '<span class="next"><img src="../images/right.svg" class="svg next-slick" alt="1"></span>',
-    prevArrow: '<span class="prev"><img src="../images/left.svg" class="svg prev-slick" alt="1"></span>',
+    nextArrow: '<span class="next"><svg class="svg next-slick"><use xlink:href="#right-arrow"/></svg></span>',
+    prevArrow: '<span class="prev"> <svg class="svg prev-slick"><use xlink:href="#left-arrow"/></svg></span>',
   });
+
+ 
 
   $('.multiple-items').slick({
     infinite: true,
     slidesToShow: 3,
     arrows: true,
-    nextArrow: '<span class="next-slick"><img src="../images/right-arrow-bot-slick.svg" class="svg next-slick" alt="1"></span>',
-    prevArrow: '<span class="prev-slick"><img src="../images/left-arrow-bot-slick.svg" class="svg prev-slick" alt="1"></span>',
+    nextArrow: '<span class="next-slick"><svg class="svg next-slick"><use xlink:href="#right-bot-arrow"/></svg></span>',
+    prevArrow: '<span class="prev-slick"><svg class="svg prev-slick"><use xlink:href="#left-bot-arrow"/></svg></span>',
     dots: false,
     slidesToScroll: 3,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,        
+          slidesToShow: 2,
+          slidesToScroll: 2,        
         }
       },
       {
         breakpoint: 768,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
           slidesToShow: 1,
           slidesToScroll: 1
         }
-      }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
+      },  
     ]
   });
 
@@ -80,39 +70,4 @@ $(document).ready(function () {
   });
 
   // End of styles for Slick sliders
-
-
-    /*
-     * Replace all SVG images with inline SVG
-     */
-    jQuery('img.svg').each(function(){
-      var $img = jQuery(this);
-      var imgID = $img.attr('id');
-      var imgClass = $img.attr('class');
-      var imgURL = $img.attr('src');
-
-      jQuery.get(imgURL, function(data) {
-          // Get the SVG tag, ignore the rest
-          var $svg = jQuery(data).find('svg');
-
-          // Add replaced image's ID to the new SVG
-          if(typeof imgID !== 'undefined') {
-              $svg = $svg.attr('id', imgID);
-          }
-          // Add replaced image's classes to the new SVG
-          if(typeof imgClass !== 'undefined') {
-              $svg = $svg.attr('class', imgClass+' replaced-svg');
-          }
-
-          // Remove any invalid XML tags as per http://validator.w3.org
-          $svg = $svg.removeAttr('xmlns:a');
-
-          // Replace image with new SVG
-          $img.replaceWith($svg);
-
-      }, 'xml');
-
-  });
-
-
 });
